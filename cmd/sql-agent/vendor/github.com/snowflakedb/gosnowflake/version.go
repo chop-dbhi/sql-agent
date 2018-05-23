0d6a4832b7952e4ad2e9fbbcedc981ae855931c7
Byron Ruth <b@devel.io>
2018-05-23 07:07:07 -0400

    Remove vendor directory
    Signed-off-by: Byron Ruth <b@devel.io>

--- cmd/sql-agent/vendor/github.com/snowflakedb/gosnowflake/version.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2017-2018 Snowflake Computing Inc. All right reserved.
-
-package gosnowflake
-
-// SnowflakeGoDriverVersion is the version of Go Snowflake Driver.
-const SnowflakeGoDriverVersion = "1.1.6"
